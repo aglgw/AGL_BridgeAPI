76505e00f72e19b7c1987721af16bf0063775943
함상명 <sm.ham@aglgw.com>
2024-11-06 18:18:18 +0900

    프로젝트 명칭 변경 및 폴더 경로 수정

---
+++ Bridge_API/Interfaces/IBackgroundTaskQueue.cs
@@ -0,0 +1,8 @@
+﻿namespace AGL.Api.Bridge_API.Interfaces
+{
+    public interface IBackgroundTaskQueue
+    {
+        void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem);
+        Task<Func<CancellationToken, Task>> DequeueAsync(CancellationToken cancellationToken);
+    }
+}
